7ff0e91faa02b29e7b037f0911edd39a56ea8d01
Christiannguyen1 <110247128+Christiannguyen1@users.noreply.github.com>
2025-01-13 16:44:09 -0600

    Interaction key
    Added interaction key 'e'

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -10,6 +10,33 @@ public class Player : MonoBehaviour
 
     private bool isWalking;
     private Vector3 lastInteractedDir;
+
+    private void Start()
+    {
+        gameInput.OnInteractAction += GameInput_OnInteractAction;
+    }
+    
+    private void GameInput_OnInteractAction(object sender, System.EventArgs e)
+    {
+        Vector2 inputVector = gameInput.GetMovementVectorNormalized();
+        
+        Vector3 moveDir = new Vector3(inputVector.x, 0f, inputVector.y);
+
+        if (moveDir != Vector3.zero)
+        {
+            lastInteractedDir = moveDir;
+        }
+        
+        float interactDistance = 2f;
+
+        if (Physics.Raycast(transform.position, lastInteractedDir, out RaycastHit raycastHit, interactDistance, countersLayerMask))
+        {
+            if (raycastHit.transform.TryGetComponent(out ClearCounter clearCounter))
+            {
+
+            }
+        }
+    }
     
     private void Update()
     {
