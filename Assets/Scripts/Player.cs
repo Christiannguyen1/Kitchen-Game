194c1d52317a7b158db2313a3d05bf7fd506e7a9
Christiannguyen1 <110247128+Christiannguyen1@users.noreply.github.com>
2025-01-16 16:33:48 -0600

    Updated Counter Script
    Added a highlight

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -10,6 +10,7 @@ public class Player : MonoBehaviour
 
     private bool isWalking;
     private Vector3 lastInteractedDir;
+    private ClearCounter selectedCounter;
 
     private void Start()
     {
@@ -18,24 +19,13 @@ public class Player : MonoBehaviour
     
     private void GameInput_OnInteractAction(object sender, System.EventArgs e)
     {
-        Vector2 inputVector = gameInput.GetMovementVectorNormalized();
-        
-        Vector3 moveDir = new Vector3(inputVector.x, 0f, inputVector.y);
 
-        if (moveDir != Vector3.zero)
+        if (selectedCounter != null)
         {
-            lastInteractedDir = moveDir;
+            selectedCounter.Interact();
         }
         
-        float interactDistance = 2f;
-
-        if (Physics.Raycast(transform.position, lastInteractedDir, out RaycastHit raycastHit, interactDistance, countersLayerMask))
-        {
-            if (raycastHit.transform.TryGetComponent(out ClearCounter clearCounter))
-            {
-
-            }
-        }
+        
     }
     
     private void Update()
@@ -66,8 +56,19 @@ public class Player : MonoBehaviour
         {
             if (raycastHit.transform.TryGetComponent(out ClearCounter clearCounter))
             {
-                clearCounter.Interact();
+                if (clearCounter != selectedCounter)
+                {
+                    selectedCounter = clearCounter;
+                }
             }
+            else
+            {
+                selectedCounter = null;
+            }
+        }
+        else
+        {
+            selectedCounter = null;
         }
 
     }
